71e2564657ccb8099fabdc1ca0a7280e81e282b6
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-02-01 06:32:27 +0300

    Update CompilationRepository.java

--- main-service/src/main/java/ru/practicum/compilations/repository/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/compilations/repository/CompilationRepository.java
@@ -1,18 +1,15 @@
 package ru.practicum.compilations.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.practicum.compilations.model.Compilation;
 
+import java.util.List;
+
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
-    Page<Compilation> findAll(Pageable pageable);
-
-    Page<Compilation> findByPinned(Boolean pinned, Pageable pageable);
-
-    @Query("SELECT c FROM Compilation c " +
-            "WHERE (:pinned IS NULL OR c.pinned = :pinned)")
-    Page<Compilation> findAllWithFilter(@Param("pinned") Boolean pinned, Pageable pageable);
+    
+    @Query("SELECT c FROM Compilation c WHERE :pinned IS NULL OR c.pinned = :pinned")
+    List<Compilation> findByPinned(Boolean pinned, Pageable pageable);
 }
