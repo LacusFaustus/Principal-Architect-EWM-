8644a650fbd6f1176c83b09bed241937297a0028
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-02-01 06:45:04 +0300

    Update CompilationMapper.java

--- main-service/src/main/java/ru/practicum/compilations/dto/CompilationMapper.java
+++ main-service/src/main/java/ru/practicum/compilations/dto/CompilationMapper.java
@@ -1,17 +1,10 @@
 package ru.practicum.compilations.dto;
 
 import org.mapstruct.*;
-import ru.practicum.category.dto.CategoryDto;
 import ru.practicum.compilations.model.Compilation;
 import ru.practicum.event.dto.EventShortDto;
-import ru.practicum.event.mapper.EventMapper;
-import ru.practicum.event.model.Event;
-import ru.practicum.user.dto.UserShortDto;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 
 @Mapper(componentModel = "spring")
 public interface CompilationMapper {
