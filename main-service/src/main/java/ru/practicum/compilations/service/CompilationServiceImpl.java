2cd96134c8d1571bc264504adc2d1802120e89f9
Kruglov Vanya <120953076+MadokaKaname42@users.noreply.github.com>
2026-02-01 06:37:23 +0300

    Update CompilationServiceImpl.java

--- main-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
@@ -11,7 +11,7 @@ import ru.practicum.compilations.dto.CompilationDto;
 import ru.practicum.compilations.dto.CompilationSearchParam;
 import ru.practicum.compilations.dto.NewCompilationDto;
 import ru.practicum.compilations.dto.UpdateCompilationRequest;
-import ru.practicum.compilations.mapper.CompilationMapper;
+import ru.practicum.compilations.dto.CompilationMapper;
 import ru.practicum.compilations.model.Compilation;
 import ru.practicum.compilations.repository.CompilationRepository;
 import ru.practicum.dto.ViewStatsDto;
