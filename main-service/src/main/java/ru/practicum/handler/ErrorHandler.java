a693fae6af0b4da42744ab562fcff08c816b80dd
madoka <obivanyakenobi@gmail.com>
2026-02-02 04:31:57 +0300

    Рефакторинг Event+Handler

--- main-service/src/main/java/ru/practicum/handler/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/handler/ErrorHandler.java
@@ -1,13 +1,14 @@
 package ru.practicum.handler;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import ru.practicum.handler.exception.BadRequestException;
 import ru.practicum.handler.exception.ConflictException;
 import ru.practicum.handler.exception.NotFoundException;
@@ -53,18 +54,22 @@ public class ErrorHandler {
     }
 
     // 400: Нарушение валидации или условий запроса
-    @ExceptionHandler({BadRequestException.class,
+    @ExceptionHandler({
             MethodArgumentNotValidException.class,
-            MissingServletRequestParameterException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+            ConstraintViolationException.class,
+            BadRequestException.class,
+            HandlerMethodValidationException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // Устанавливаем статус 400
     public ApiError handleBadRequest(final Exception e) {
         log.error("400 Bad Request: {}", e.getMessage());
+
         return new ApiError(
                 getStackTrace(e),
                 e.getMessage(),
-                "Incorrectly made request.",
+                "Incorrectly made request.", // Reason из спецификации EWM
                 HttpStatus.BAD_REQUEST.name(),
-                LocalDateTime.now().format(FORMATTER)
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
         );
     }
 
